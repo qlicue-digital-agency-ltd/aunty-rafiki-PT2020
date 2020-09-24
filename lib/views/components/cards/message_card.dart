f24e44a1a1493ca4bb7d4541660258d2adc6aaf1
admin <kalrobbynson@gmail.com>
2020-09-24 18:11:21 +0300

    message tile

--- lib/views/components/cards/message_card.dart
+++ lib/views/components/cards/message_card.dart
@@ -2,64 +2,6 @@ import 'package:aunty_rafiki/models/message.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class MessageCard extends StatelessWidget {
-  final Message message;
-
-  const MessageCard({Key key, @required this.message}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 10.0),
-      child: Row(
-        children: [
-          message.sentByMe ? Spacer() : Container(),
-          Material(
-            child: Stack(
-              children: [
-                Container(
-                    padding: EdgeInsets.all(10),
-                    color: Colors.white,
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          message.sender == null
-                              ? Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: <Widget>[
-                                    Text(message.phoneNumber),
-                                    SizedBox(
-                                      width: 50,
-                                    ),
-                                    Text(message.userName)
-                                  ],
-                                )
-                              : Text(message.sender),
-                          Text(message.text),
-                          SizedBox(height: 14)
-                        ])),
-                Positioned(
-                  bottom: 0,
-                  right: 0,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      message.date.toString(),
-                      style: TextStyle(color: Colors.black38),
-                      textAlign: TextAlign.end,
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
 class MessageTile extends StatelessWidget {
   final Message message;
 
